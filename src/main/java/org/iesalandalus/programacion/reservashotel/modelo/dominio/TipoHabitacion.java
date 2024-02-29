d55ac1287a177e459d4a894584cdc6ec42e2cfda
Patricia <patriciatorres83@gmail.com>
2024-02-29 21:39:19 +0100

    Actualización

--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/TipoHabitacion.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/dominio/TipoHabitacion.java
@@ -1,36 +1,35 @@
 package org.iesalandalus.programacion.reservashotel.modelo.dominio;
 
 public enum TipoHabitacion {
-
-    SUITE("SUITE", 4),
-
-    SIMPLE("SIMPLE",1),
-
-    DOBLE("DOBLE",2),
-
-    TRIPLE("TRIPLE",3);
-
+    ;
 
     //Atributos
-    private String descripcion;
-    private int numeroMaximoPersonas;
+    private String cadenaAmostrar;
+
+    SUITE("SUITE"),
+
+    SIMPLE("SIMPLE"),
+
+    DOBLE("DOBLE"),
+
+    TRIPLE("TRIPLE");
+
+
+
 
     //Constructor
-    TipoHabitacion(String descripcion,int numeroMaximoPersonas) {
-        this.descripcion = descripcion;
-        this.numeroMaximoPersonas = numeroMaximoPersonas;
+    TipoHabitacion(String cadenaAmostrar) {
+        this.cadenaAmostrar = cadenaAmostrar;
     }
 
-    //Método Get
-    public int getNumeroMaximoPersonas() {
-        return numeroMaximoPersonas;
-    }
 
     //Método to String
 
+
     @Override
     public String toString() {
-        return String.format("Tipo de Habitación:%s, número de personas: %s",
-        this.descripcion, getNumeroMaximoPersonas());
+        return "TipoHabitacion{" +
+                "cadenaAmostrar='" + cadenaAmostrar + '\'' +
+                '}';
     }
 }
